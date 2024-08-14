9bc112ba732499cf3fb20e62abfb7e50a8915f00
Code Class Ngoc <ngocnhip.96@gmail.com>
2024-08-14 14:03:55 +0700

    New 2.03

--- RTE/Inc/Rte_PDC_Control.h
+++ RTE/Inc/Rte_PDC_Control.h
@@ -19,8 +19,8 @@
 /* function prototype declarations                                            */
 /*----------------------------------------------------------------------------*/
 
-extern FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_ParkDistanceControl_R_RP_DistanceUS_ReadDistance( VAR(ParkDistanceControl_uint8, AUTOMATIC, RTE_APPL_DATA) distance );
-#define Rte_Read_R_RP_DistanceUS_ReadDistance Rte_Read_ParkDistanceControl_R_RP_DistanceUS_ReadDistance 
+extern FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_ParkDistanceControl_R_RP_DistanceUS_ReadDistanceUS( VAR(ParkDistanceControl_uint8, AUTOMATIC, RTE_APPL_DATA) distanceUS );
+#define Rte_Read_R_RP_DistanceUS_ReadDistanceUS Rte_Read_ParkDistanceControl_R_RP_DistanceUS_ReadDistanceUS 
 
 extern FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Write_ParkDistanceControl_P_PP_Alarm_SendDistancetoAlarm( VAR(ParkDistanceControl_uint8, AUTOMATIC, RTE_APPL_DATA) distance_to_alarm );
 #define Rte_Write_P_PP_Alarm_SendDistancetoAlarm Rte_Write_ParkDistanceControl_P_PP_Alarm_SendDistancetoAlarm 
@@ -34,8 +34,6 @@ extern FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Write_ParkDistanceCont
 FUNC(void, AUTOMATIC) ProcessControl( VAR(void, AUTOMATIC) );
 #define RTE_RUNNABLE_Run_ProcessControl ProcessControl
 
-FUNC(void, AUTOMATIC) PDCService( VAR(void, AUTOMATIC) );
-#define RTE_RUNNABLE_Run_PDCService PDCService
 
 #endif /* RTE_PDC_CONTROL_H */
 
--- RTE/Src/Rte_PDC_Control.c
+++ RTE/Src/Rte_PDC_Control.c
@@ -22,37 +22,40 @@
 /* functions and function style macros                                        */
 /*----------------------------------------------------------------------------*/
 
-extern FUNC(Std_ReturnType, IoHwAb_CODE) IoHwAb_SendAlarmWarning( VAR(AlarmSystem_IoHwAb_AlarmLevel, AUTOMATIC) );
+/******************************************************************************/
+/*
+*/
+/******************************************************************************/
 
+extern FUNC(Std_ReturnType, IoHwAb_CODE) IoHwAb_ReadDistanceUS( VAR(AlarmSystem_uint8, AUTOMATIC, RTE_APPL_DATA) );
 
-FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Call_AlarmSystem_R_IO_WarningAlarm( VAR(AlarmSystem_IoHwAb_AlarmLevel, AUTOMATIC) alarm_warning) {
+FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_ParkDistanceControl_R_RP_DistanceUS_ReadDistanceUS( VAR(ParkDistanceControl_uint8, AUTOMATIC, RTE_APPL_DATA) distanceUS ) {
     VAR(Std_ReturnType, AUTOMATIC) return_value;
 
-    return_value = IoHwAb_SendAlarmWarning( alarm_warning );
+    return_value = IoHwAb_ReadDistanceUS( distanceUS );
     return return_value;
-}
-
+} 
 
 /******************************************************************************/
 /*
 */
 /******************************************************************************/
 
-extern FUNC(Std_ReturnType, IoHwAb_CODE) IoHwAb_ReadAlarmLevel( VAR(AlarmSystem_uint8, AUTOMATIC, RTE_APPL_DATA) );
+extern FUNC(Std_ReturnType, IoHwAb_CODE) IoHwAb_WriteDistancetoAlarm( VAR(AlarmSystem_uint8, AUTOMATIC, RTE_APPL_DATA) );
 
-FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_AlarmSystem_R_RP_Alarm_ReadAlarmLevel( VAR(AlarmSystem_uint8, AUTOMATIC, RTE_APPL_DATA) alarm_level ) {
+FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Write_ParkDistanceControl_P_PP_Alarm_SendDistancetoAlarm( VAR(ParkDistanceControl_uint8, AUTOMATIC, RTE_APPL_DATA) distance_to_alarm ) {
     VAR(Std_ReturnType, AUTOMATIC) return_value;
 
-    return_value = IoHwAb_Q_SensorGet( alarm_level );
+    return_value = IoHwAb_WriteDistancetoAlarm( distance_to_alarm );
     return return_value;
 }
 
 
 /**********************************  Section 2 ******************************* */
-extern FUNC(void, AppAlarm_CODE) ExecuteAlarmCommand( VAR(void, AUTOMATIC) );
+extern FUNC(void, AppAlarm_CODE) ProcessControl( VAR(void, AUTOMATIC) );
 
-FUNC(void, RTE_CODE_EcucPartition_0) Rte_ExecuteAlarmCommand( VAR(void, AUTOMATIC) ) {
+FUNC(void, RTE_CODE_EcucPartition_0) Rte_ProcessControl( VAR(void, AUTOMATIC) ) {
 
-    ExecuteAlarmCommand();
+    PDCService();
 
 }
