4aad6b123ecc01a6635e9e16103ba7fe378f90f5
dotiendunga <dungdo3171@gmail.com>
2024-08-14 15:35:25 +0700

    a

--- RTE/Inc/Rte_PDC_Service.h
+++ RTE/Inc/Rte_PDC_Service.h
@@ -22,11 +22,11 @@
 
 
 
-FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_PDCService_R_RP_PDCService_StartPDCControl( VAR(PackDistanceControl_Service_uint8, AUTOMATIC) Start_PDCControl) 
+FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Read_PDCService_R_RP_PDCService_StartPDCControl( P2VAR(PackDistanceControl_Service_uint8, AUTOMATIC) Start_PDCControl);
 #define Rte_Read_R_PP_PDCService_StartPDCControl Rte_Read_PDCService_R_RP_PDCService_StartPDCControl 
 
-FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Call_PDCService_R_IO_PDCStatus_GetPDCStatus( VAR(PDCService_Status_uint8, AUTOMATIC) getPDCStatus );
-#define Rte_Call_R_IO_GetPDCStatus Rte_Call_PDCService_R_IO_PDCStatus_GetPDCStatus 
+FUNC(Std_ReturnType, RTE_CODE_EcucPartition_0) Rte_Call_PDCService_R_IO_PDCStatus_SendPDCStatus( VAR(PDCService_Status_uint8, AUTOMATIC) sendPDCStatus );
+#define Rte_Call_R_IO_SendPDCStatus Rte_Call_PDCService_R_IO_PDCStatus_SendPDCStatus 
 
 
 
